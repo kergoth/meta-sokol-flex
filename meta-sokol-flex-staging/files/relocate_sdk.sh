f87d8c43982f38c6a2d85fe8faf989993424df40
Christopher Larson <chris_larson@mentor.com>
2023-01-06 22:33:20 +0500

    relocate_sdk.sh: remove SUDO_EXEC usage
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

--- meta-sokol-flex-staging/files/relocate_sdk.sh
+++ meta-sokol-flex-staging/files/relocate_sdk.sh
@@ -26,13 +26,13 @@ if [ -n "$real_env_setup_script" ] ; then
 fi
 
 # fix dynamic loader paths in all ELF SDK binaries
-native_sysroot=$($SUDO_EXEC cat $env_setup_script |grep 'OECORE_NATIVE_SYSROOT='|cut -d'=' -f2|tr -d '"')
-dl_path=$($SUDO_EXEC find $native_sysroot/lib -maxdepth 1 -name "ld-linux*")
+native_sysroot=$(cat $env_setup_script |grep 'OECORE_NATIVE_SYSROOT='|cut -d'=' -f2|tr -d '"')
+dl_path=$(find $native_sysroot/lib -maxdepth 1 -name "ld-linux*")
 if [ "$dl_path" = "" ] ; then
 	echo "SDK could not be set up. Relocate script unable to find ld-linux.so. Abort!"
 	exit 1
 fi
-executable_files=$($SUDO_EXEC find $native_sysroot -type f \
+executable_files=$(find $native_sysroot -type f \
 	\( -perm -0100 -o -perm -0010 -o -perm -0001 \) -printf "'%h/%f' ")
 if [ "x$executable_files" = "x" ]; then
    echo "SDK relocate failed, could not get executalbe files"
@@ -62,14 +62,14 @@ fi
 # replace @SDKPATH@ with the new prefix in all text files: configs/scripts/etc.
 # replace the host perl with SDK perl.
 for replace in "$target_sdk_dir -maxdepth 1" "$native_sysroot"; do
-	$SUDO_EXEC find $replace -type f
+	find $replace -type f
 done | xargs -n100 file | grep ":.*\(ASCII\|script\|source\).*text" | \
     awk -F': ' '{printf "\"%s\"\n", $1}' | \
     grep -Fv -e "$target_sdk_dir/environment-setup-" \
              -e "$target_sdk_dir/relocate_sdk" \
              -e "$target_sdk_dir/post-relocate-setup" \
              -e "$target_sdk_dir/${0##*/}" | \
-    xargs -n100 $SUDO_EXEC sed -i \
+    xargs -n100 sed -i \
         -e "s:$SDK_BUILD_PATH:$target_sdk_dir:g" \
         -e "s:^#! */usr/bin/perl.*:#! /usr/bin/env perl:g" \
         -e "s: /usr/bin/perl: /usr/bin/env perl:g"
@@ -80,8 +80,8 @@ if [ $? -ne 0 ]; then
 fi
 
 # change all symlinks pointing to @SDKPATH@
-for l in $($SUDO_EXEC find $native_sysroot -type l); do
-	$SUDO_EXEC ln -sfn $(readlink $l|$SUDO_EXEC sed -e "s:$SDK_BUILD_PATH:$target_sdk_dir:") $l
+for l in $(find $native_sysroot -type l); do
+	ln -sfn $(readlink $l|sed -e "s:$SDK_BUILD_PATH:$target_sdk_dir:") $l
 	if [ $? -ne 0 ]; then
 		echo "Failed to setup symlinks. Relocate script failed. Abort!"
 		exit 1
